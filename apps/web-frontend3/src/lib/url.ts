8a18fb13b139a79f4abbdd8b5b18f74a0ecf9560
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2026-06-20 20:14:33 +0100

    refactor(web-frontend); delete both v2 and v3

--- apps/web-frontend3/src/lib/url.ts
+++
@@ -1,5 +0,0 @@
-export function normalizeRoute(route: string) {
-    if (!route.startsWith('/')) return `/${route}`
-    if (route !== '/' && route.endsWith('/')) return route.slice(0, -1)
-    return route
-}
